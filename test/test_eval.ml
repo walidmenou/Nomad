b7161aec4ee9d97cd27e081644e610701ee29f28
Walid Menou <walid.menou@outlook.com>
2026-07-28 20:24:35 -0400

    style: add .ocamlformat and format the tree

--- test/test_eval.ml
+++ test/test_eval.ml
@@ -8,29 +8,38 @@ let check_eval env expr expected () =
   | exception e -> Alcotest.fail ("Unknown exception: " ^ Printexc.to_string e)
 
 let test_list () =
-  check_eval [] (List [Int 1; Int 2; Int 3]) (List [Int 1; Int 2; Int 3]) ()
+  check_eval [] (List [ Int 1; Int 2; Int 3 ]) (List [ Int 1; Int 2; Int 3 ]) ()
 
 let test_match_wildcard () =
-  check_eval [] (Match (Int 1, [(PatWildcard, Int 2)])) (Int 2) ()
+  check_eval [] (Match (Int 1, [ (PatWildcard, Int 2) ])) (Int 2) ()
 
 let test_match_var () =
-  check_eval [] (Match (Int 1, [(PatVar "x", Var "x")])) (Int 1) ()
+  check_eval [] (Match (Int 1, [ (PatVar "x", Var "x") ])) (Int 1) ()
 
 let test_match_int () =
-  check_eval [] (Match (Int 1, [(PatInt 1, Int 2); (PatWildcard, Int 3)])) (Int 2) ();
-  check_eval [] (Match (Int 4, [(PatInt 1, Int 2); (PatWildcard, Int 3)])) (Int 3) ()
+  check_eval []
+    (Match (Int 1, [ (PatInt 1, Int 2); (PatWildcard, Int 3) ]))
+    (Int 2) ();
+  check_eval []
+    (Match (Int 4, [ (PatInt 1, Int 2); (PatWildcard, Int 3) ]))
+    (Int 3) ()
 
 let test_match_bool () =
-  check_eval [] (Match (Bool true, [(PatBool true, Int 2); (PatWildcard, Int 3)])) (Int 2) ()
+  check_eval []
+    (Match (Bool true, [ (PatBool true, Int 2); (PatWildcard, Int 3) ]))
+    (Int 2) ()
 
 let test_match_string () =
-  check_eval [] (Match (String "hello", [(PatString "hello", Int 2); (PatWildcard, Int 3)])) (Int 2) ()
+  check_eval []
+    (Match (String "hello", [ (PatString "hello", Int 2); (PatWildcard, Int 3) ]))
+    (Int 2) ()
 
-let tests = [
-  "list", `Quick, test_list;
-  "match_wildcard", `Quick, test_match_wildcard;
-  "match_var", `Quick, test_match_var;
-  "match_int", `Quick, test_match_int;
-  "match_bool", `Quick, test_match_bool;
-  "match_string", `Quick, test_match_string;
-]
+let tests =
+  [
+    ("list", `Quick, test_list);
+    ("match_wildcard", `Quick, test_match_wildcard);
+    ("match_var", `Quick, test_match_var);
+    ("match_int", `Quick, test_match_int);
+    ("match_bool", `Quick, test_match_bool);
+    ("match_string", `Quick, test_match_string);
+  ]
--- test/test_parser.ml
+++ test/test_parser.ml
@@ -3,7 +3,8 @@ open Nomad.Parser
 
 let check_parse p input expected () =
   match run p input with
-  | Ok actual -> Alcotest.(check bool) "matches expected" true (actual = expected)
+  | Ok actual ->
+      Alcotest.(check bool) "matches expected" true (actual = expected)
   | Error e -> Alcotest.fail e
 
 let check_fail p input () =
@@ -27,25 +28,32 @@ let test_var_expr () =
 
 let test_arith_expr () =
   check_parse expr "1 + 2" (BinOp (Int 1, Add, Int 2)) ();
-  check_parse expr "1 * 2 + 3" (BinOp (BinOp (Int 1, Mul, Int 2), Add, Int 3)) ();
-  check_parse expr "1 + 2 * 3" (BinOp (Int 1, Add, BinOp (Int 2, Mul, Int 3))) ()
+  check_parse expr "1 * 2 + 3"
+    (BinOp (BinOp (Int 1, Mul, Int 2), Add, Int 3))
+    ();
+  check_parse expr "1 + 2 * 3"
+    (BinOp (Int 1, Add, BinOp (Int 2, Mul, Int 3)))
+    ()
 
 let test_cmp_expr () =
   check_parse expr "1 < 2" (BinOp (Int 1, Less, Int 2)) ();
   check_parse expr "1 <= 2" (BinOp (Int 1, Leq, Int 2)) ();
-  check_parse expr "1 < 2 < 3" (BinOp (BinOp (Int 1, Less, Int 2), And, BinOp (Int 2, Less, Int 3))) ()
+  check_parse expr "1 < 2 < 3"
+    (BinOp (BinOp (Int 1, Less, Int 2), And, BinOp (Int 2, Less, Int 3)))
+    ()
 
 let test_let_expr () =
   check_parse expr "let x = 1 in x" (Let ("x", Int 1, Var "x")) ()
 
 let test_let_rec_expr () =
-  check_parse expr "let rec f = fun x -> x in f" (Rec ("f", Fun ("x", Var "x"), Var "f")) ()
+  check_parse expr "let rec f = fun x -> x in f"
+    (Rec ("f", Fun ("x", Var "x"), Var "f"))
+    ()
 
 let test_if_expr () =
   check_parse expr "if true then 1 else 0" (If (Bool true, Int 1, Int 0)) ()
 
-let test_fun_expr () =
-  check_parse expr "fun x -> x" (Fun ("x", Var "x")) ()
+let test_fun_expr () = check_parse expr "fun x -> x" (Fun ("x", Var "x")) ()
 
 let test_app_expr () =
   check_parse expr "f x" (App (Var "f", Var "x")) ();
@@ -56,7 +64,9 @@ let test_pipe_expr () =
   check_parse expr "x |> f |> g" (App (Var "g", App (Var "f", Var "x"))) ()
 
 let test_match_expr () =
-  check_parse expr "match x with _ -> 1 | y -> 2" (Match (Var "x", [(PatWildcard, Int 1); (PatVar "y", Int 2)])) ()
+  check_parse expr "match x with _ -> 1 | y -> 2"
+    (Match (Var "x", [ (PatWildcard, Int 1); (PatVar "y", Int 2) ]))
+    ()
 
 let test_pattern () =
   check_parse pattern "_" PatWildcard ();
@@ -67,26 +77,34 @@ let test_pattern () =
 
 let test_statement () =
   check_parse statement "let x = 1" (LetStmt ("x", Int 1)) ();
-  check_parse statement "let rec f = fun x -> x" (RecStmt ("f", Fun ("x", Var "x"))) ();
+  check_parse statement "let rec f = fun x -> x"
+    (RecStmt ("f", Fun ("x", Var "x")))
+    ();
   check_parse statement "1 + 1" (ExprStmt (BinOp (Int 1, Add, Int 1))) ()
 
 let test_program () =
-  check_parse program "let x = 1 let y = 2 if true then x + y else 0" 
-    [LetStmt ("x", Int 1); LetStmt ("y", Int 2); ExprStmt (If (Bool true, BinOp (Var "x", Add, Var "y"), Int 0))] ()
+  check_parse program "let x = 1 let y = 2 if true then x + y else 0"
+    [
+      LetStmt ("x", Int 1);
+      LetStmt ("y", Int 2);
+      ExprStmt (If (Bool true, BinOp (Var "x", Add, Var "y"), Int 0));
+    ]
+    ()
 
-let tests = [
-  "literals", `Quick, test_lit_expr;
-  "variables", `Quick, test_var_expr;
-  "arithmetic", `Quick, test_arith_expr;
-  "comparisons", `Quick, test_cmp_expr;
-  "let", `Quick, test_let_expr;
-  "let rec", `Quick, test_let_rec_expr;
-  "if", `Quick, test_if_expr;
-  "functions", `Quick, test_fun_expr;
-  "applications", `Quick, test_app_expr;
-  "pipe", `Quick, test_pipe_expr;
-  "match", `Quick, test_match_expr;
-  "patterns", `Quick, test_pattern;
-  "statements", `Quick, test_statement;
-  "programs", `Quick, test_program;
-]
+let tests =
+  [
+    ("literals", `Quick, test_lit_expr);
+    ("variables", `Quick, test_var_expr);
+    ("arithmetic", `Quick, test_arith_expr);
+    ("comparisons", `Quick, test_cmp_expr);
+    ("let", `Quick, test_let_expr);
+    ("let rec", `Quick, test_let_rec_expr);
+    ("if", `Quick, test_if_expr);
+    ("functions", `Quick, test_fun_expr);
+    ("applications", `Quick, test_app_expr);
+    ("pipe", `Quick, test_pipe_expr);
+    ("match", `Quick, test_match_expr);
+    ("patterns", `Quick, test_pattern);
+    ("statements", `Quick, test_statement);
+    ("programs", `Quick, test_program);
+  ]
