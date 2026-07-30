817c6c99b642a557b655cf280a7775326ee50109
Walid Menou <walid.menou@outlook.com>
2026-07-29 21:11:29 -0400

    docs: add comprehension examples

--- test/test_program.ml
+++ test/test_program.ml
@@ -370,6 +370,20 @@ let test_type_errors () =
 let test_examples () =
   check_run (read_file "../examples/gcd.nd") [ "<fun>"; "6" ] ();
   check_run
+    (read_file "../examples/triples.nd")
+    [
+      "<fun>";
+      "[[3; 4; 5]; [6; 8; 10]; [5; 12; 13]; [9; 12; 15]; [8; 15; 17]; [12; 16; \
+       20]]";
+    ]
+    ();
+  check_run
+    (read_file "../examples/primes.nd")
+    [
+      "<fun>"; "<fun>"; "<fun>"; "<fun>"; "[2; 3; 5; 7; 11; 13; 17; 19; 23; 29]";
+    ]
+    ();
+  check_run
     (read_file "../examples/sort.nd")
     [ "<fun>"; "<fun>"; "[1; 2; 5; 5; 6; 9]" ]
     ()
